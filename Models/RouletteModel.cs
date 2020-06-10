40d205f486121c4fe5448591e1454bee5f65bae1
kevinfcc26 <kevinfcc26@gmail.com>
2020-06-10 13:08:23 -0500

    Creating the RouletteModel

---
+++ Models/RouletteModel.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RouletteApi.Models
+{
+    public class RouletteModel
+    {
+        public int id { get; set; }
+        public bool open { get; set;}
+    }
+}
